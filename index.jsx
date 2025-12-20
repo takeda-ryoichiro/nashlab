08773a150ce72f7d11942d6a2607af60f6152a84
NashLab <61621559+takeda-ryoichiro@users.noreply.github.com>
2025-12-20 15:23:20 +0900

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PreflopQuizApp from "/App.jsx";
+import "/index.css";
+
+var _jsxFileName = "C:/Users/taked/NashLab/src/main.jsx";
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PreflopQuizApp />
+  </React.StrictMode>
+);
