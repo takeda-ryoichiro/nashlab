06669bf3eced0f3c6d5cf2459a4be22e7b7220e8
NashLab <61621559+takeda-ryoichiro@users.noreply.github.com>
2025-12-26 16:31:23 +0900

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import PreflopQuizApp from "/App.jsx";
+import Home from "./home.jsx";
+
 import "/index.css";
 
 var _jsxFileName = "C:/Users/taked/NashLab/src/main.jsx";
@@ -9,6 +11,10 @@ import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PreflopQuizApp />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="quiz" element={<PreflopQuizApp />}></Route>      
+    </Routes>
   </React.StrictMode>
+
 );
